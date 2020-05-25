14cb54325306680fa4e11d54f9fb854da7dec7a3
David Brennan <davidwbrennan@hotmail.com>
2020-05-25 16:45:48 +0100

    Add an authors controller with get all and get.

---
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -0,0 +1,36 @@
+﻿using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CourseLibrary.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
+        {
+            _courseLibraryRepository = courseLibraryRepository ??
+                throw new ArgumentException(nameof(courseLibraryRepository));
+        }
+
+        [HttpGet()]
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _courseLibraryRepository.GetAuthors();
+            return new JsonResult(authorsFromRepo);
+            //return new JsonResult(_courseLibraryRepository.GetAuthors());
+        }
+
+        //[HttpGet("{authorId:guid}")]
+        [HttpGet("{authorId}")]
+        public IActionResult GetAuthor(Guid authorId)
+        {
+            var authorFromRepo = _courseLibraryRepository.GetAuthor(authorId);
+            return new JsonResult(authorFromRepo);
+            //return new JsonResult(_courseLibraryRepository.GetAuthor(authorId));
+        }
+    }
+}
